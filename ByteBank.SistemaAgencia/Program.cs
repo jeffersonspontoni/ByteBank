2a1df2b3a7dbc54cf3a130af46df3d9dbbeb7b57
Jefferson <jeffersonspontoni@gmail.com>
2020-07-04 16:15:51 -0300

    Primeiro commit do projeto

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente(847, 489754);
+
+            Console.WriteLine(contaCorrente.Numero);
+            Console.ReadLine();
+        }
+    }
+}
